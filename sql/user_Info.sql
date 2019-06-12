d6a68c8756cf670ad5cb8b44b294c754efc28df0
Kulbirk <kbkulbir87@gmail.com>
2019-06-11 20:38:58 -0700

    Added sql folder to the root project that contains user_Info.sql file having commands for creating user_Info table

---
+++ sql/user_Info.sql
@@ -0,0 +1,27 @@
+create table user_Info (
+	id INT,
+	userName VARCHAR(50),
+	password VARCHAR(9),
+	gender VARCHAR(50),
+	city VARCHAR(9)
+);
+insert into user_Info (id, userName, password, gender, city) values (1, 'Addie', 'Nafiza1', 'Male', 'Surrey');
+insert into user_Info (id, userName, password, gender, city) values (2, 'Willy', 'Kulbir123', 'Male', 'Victoria');
+insert into user_Info (id, userName, password, gender, city) values (3, 'Bev', 'Aman123', 'Male', 'Richmond');
+insert into user_Info (id, userName, password, gender, city) values (4, 'Pauletta', 'Aman123', 'Female', 'Burnaby');
+insert into user_Info (id, userName, password, gender, city) values (5, 'Teddi', 'Aman1', 'Female', 'Victoria');
+insert into user_Info (id, userName, password, gender, city) values (6, 'Miller', 'Aman123', 'Male', 'Victoria');
+insert into user_Info (id, userName, password, gender, city) values (7, 'Orton', 'Kulbir1', 'Male', 'Burnaby');
+insert into user_Info (id, userName, password, gender, city) values (8, 'Ricki', 'Kulbir1', 'Female', 'Vancouver');
+insert into user_Info (id, userName, password, gender, city) values (9, 'Ronnie', 'Kulbir123', 'Female', 'Vancouver');
+insert into user_Info (id, userName, password, gender, city) values (10, 'Donetta', 'Aman123', 'Female', 'Surrey');
+insert into user_Info (id, userName, password, gender, city) values (11, 'Graeme', 'Nafiza123', 'Male', 'Richmond');
+insert into user_Info (id, userName, password, gender, city) values (12, 'Alix', 'Nafiza123', 'Male', 'Surrey');
+insert into user_Info (id, userName, password, gender, city) values (13, 'Allard', 'Kulbir1', 'Male', 'Burnaby');
+insert into user_Info (id, userName, password, gender, city) values (14, 'Sarah', 'Nafiza123', 'Female', 'Victoria');
+insert into user_Info (id, userName, password, gender, city) values (15, 'Robinett', 'Aman123', 'Female', 'Vancouver');
+insert into user_Info (id, userName, password, gender, city) values (16, 'Puff', 'Nafiza123', 'Male', 'Richmond');
+insert into user_Info (id, userName, password, gender, city) values (17, 'Winfield', 'Kulbir123', 'Male', 'Burnaby');
+insert into user_Info (id, userName, password, gender, city) values (18, 'Gil', 'Nafiza1', 'Male', 'Surrey');
+insert into user_Info (id, userName, password, gender, city) values (19, 'Clywd', 'Kulbir123', 'Male', 'Surrey');
+insert into user_Info (id, userName, password, gender, city) values (20, 'Peterus', 'Aman123', 'Male', 'Victoria');
