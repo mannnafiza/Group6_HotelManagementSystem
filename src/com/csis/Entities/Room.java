4f0f7f592fad20167fd6550a4eac0a1a7233626f
mannnafiza <Mannn4@student.douglascollege.ca>
2019-06-13 20:53:35 -0700

    Add Room.java

---
+++ src/com/csis/Entities/Room.java
@@ -0,0 +1,48 @@
+package com.csis.Entities;
+
+import java.util.Date;
+
+public class Room {
+	private String roomType;
+	private boolean meal;
+	private boolean addService;
+	private Date reserveDate;
+	
+	Room(){
+		
+	}
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+
+	public boolean isMeal() {
+		return meal;
+	}
+
+	public void setMeal(boolean meal) {
+		this.meal = meal;
+	}
+
+	public boolean isAddService() {
+		return addService;
+	}
+
+	public void setAddService(boolean addService) {
+		this.addService = addService;
+	}
+
+	public Date getReserveDate() {
+		return reserveDate;
+	}
+
+	public void setReserveDate(Date reserveDate) {
+		this.reserveDate = reserveDate;
+	}
+	
+	
+}
