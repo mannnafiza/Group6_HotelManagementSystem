22a6bddccfa2de562100ee179f02af4ed3cd5563
mannnafiza <Mannn4@student.douglascollege.ca>
2019-06-13 20:58:38 -0700

    Add Banquet.java

---
+++ src/com/csis/Entities/Banquet.java
@@ -0,0 +1,39 @@
+package com.csis.Entities;
+
+import java.util.Date;
+
+public class Banquet {
+	private Date date;
+	private boolean meal;
+	private boolean addService;
+	
+	Banquet(){
+		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public boolean isMeal() {
+		return meal;
+	}
+
+	public void setMeal(boolean meal) {
+		this.meal = meal;
+	}
+
+	public boolean isAddService() {
+		return addService;
+	}
+
+	public void setAddService(boolean addService) {
+		this.addService = addService;
+	}
+	
+	
+}
