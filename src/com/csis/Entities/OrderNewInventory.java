67f8a9665c20bc77604d78064f1f62931ed60b21
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-07-07 01:16:04 -0700

    Added class ordernewinventory

---
+++ src/com/csis/Entities/OrderNewInventory.java
@@ -0,0 +1,61 @@
+package com.csis.Entities;
+
+public class OrderNewInventory {
+	
+	private String Item;
+	private Integer Quantity = 0;
+	private float UnitPrice = 0;
+	private float Amount = 0;
+	
+	
+	/**
+	 * @return the item
+	 */
+	public String getItem() {
+		return Item;
+	}
+	/**
+	 * @param item the item to set
+	 */
+	public void setItem(String item) {
+		Item = item;
+	}
+	/**
+	 * @return the quantity
+	 */
+	public Integer getQuantity() {
+		return Quantity;
+	}
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(Integer quantity) {
+		Quantity = quantity;
+	}
+	/**
+	 * @return the unitPrice
+	 */
+	public float getUnitPrice() {
+		return UnitPrice;
+	}
+	/**
+	 * @param unitPrice the unitPrice to set
+	 */
+	public void setUnitPrice(float unitPrice) {
+		UnitPrice = unitPrice;
+	}
+	/**
+	 * @return the amount
+	 */
+	public float getAmount() {
+		return Amount;
+	}
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(float amount) {
+		Amount = amount;
+	}
+	
+
+}
