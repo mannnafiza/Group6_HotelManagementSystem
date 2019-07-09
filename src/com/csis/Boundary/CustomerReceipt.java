9214befa73bcae3c6fab7863cb746c3459dbb8be
Kulbirk <kbkulbir87@gmail.com>
2019-07-08 20:56:21 -0700

    Modified Customerreceipt class to add more GUI components and make use of BillingData instance to display the bill

--- src/com/csis/Boundary/CustomerReceipt.java
+++ src/com/csis/Boundary/CustomerReceipt.java
@@ -12,22 +12,24 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class CustomerReceipt {
 
 	private JFrame frame;
-	private JTextField textField_1;
 	private JLabel lblBillFor;
+	JLabel lblYourReservations;
+	JTextArea textAreaEntries;
 	BillingData bill = new BillingData();
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args, BillingData bill) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					CustomerReceipt window = new CustomerReceipt();
+					CustomerReceipt window = new CustomerReceipt(bill);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +41,8 @@ public class CustomerReceipt {
 	/**
 	 * Create the application.
 	 */
-	public CustomerReceipt() {
+	public CustomerReceipt(BillingData bill) {
+		this.bill = bill;
 		initialize();
 	}
 
@@ -48,16 +51,32 @@ public class CustomerReceipt {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 581, 462);
+		frame.setBounds(100, 100, 818, 488);
 		frame.getContentPane().setForeground(new Color(0, 0, 0));
 		frame.getContentPane().setBackground(new Color(95, 158, 160));
 		frame.getContentPane().setLayout(null);
 		
 		lblBillFor = new JLabel("Bill for");
 		lblBillFor.setForeground(new Color(255, 255, 255));
-		lblBillFor.setBounds(229, 43, 161, 29);
+		lblBillFor.setBounds(407, 11, 161, 29);
 		lblBillFor.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
 		frame.getContentPane().add(lblBillFor);
+		
+		lblYourReservations = new JLabel("Your Reservations:");
+		lblYourReservations.setBounds(32, 49, 142, 21);
+		lblYourReservations.setForeground(new Color(255, 255, 255));
+		lblYourReservations.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		frame.getContentPane().add(lblYourReservations);
+		
+		textAreaEntries = new JTextArea();
+		textAreaEntries.setForeground(new Color(0, 128, 128));
+		textAreaEntries.setBounds(32, 81, 770, 323);
+		frame.getContentPane().add(textAreaEntries);
+		
+		JButton btnPayBill = new JButton("Pay Bill");
+		btnPayBill.setForeground(new Color(0, 128, 0));
+		btnPayBill.setBounds(290, 415, 89, 23);
+		frame.getContentPane().add(btnPayBill);
 				
 		show(bill);
 		
@@ -66,5 +85,7 @@ public class CustomerReceipt {
 	private void show(BillingData bill) {
 		// TODO Auto-generated method stub
 		lblBillFor.setText("Bill for " + bill.getName());
+		textAreaEntries.setText(bill.prepareBill());
+		
 	}
 }
