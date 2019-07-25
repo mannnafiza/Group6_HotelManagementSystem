74dd5d749c28bd7aa28c5c222196869ff1ac726d
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-07-24 22:47:30 -0700

    Create the rieviews classs

---
+++ src/com/csis/Entities/Reviews.java
@@ -0,0 +1,46 @@
+package com.csis.Entities;
+
+import java.util.Vector;
+
+public class Reviews {
+
+	private Integer Id = 0;
+    private String Comment;
+	
+	
+    
+	public Integer getId() {
+		return Id;
+	}
+
+
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+
+
+	public String getComment() {
+		return Comment;
+	}
+
+
+
+	public void setComment(String comment) {
+		Comment = comment;
+	}
+
+
+
+	public Vector getVector() {
+		Vector v = new Vector();
+		
+		v.add(Id);
+		v.add(Comment);
+		
+		
+		return v;
+	}
+	
+}
