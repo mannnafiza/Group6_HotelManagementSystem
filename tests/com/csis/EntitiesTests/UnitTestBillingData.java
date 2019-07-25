366554504b60140ed74be0c0eccfa51e09171e36
Kulbirk <kbkulbir87@gmail.com>
2019-07-25 02:29:53 -0700

    Add UnitTestBillingData.java to perform unit testing on BillingData Entity class

---
+++ tests/com/csis/EntitiesTests/UnitTestBillingData.java
@@ -0,0 +1,35 @@
+package com.csis.EntitiesTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.csis.Entities.BillingData;
+
+class UnitTestBillingData {
+
+	BillingData bill = new BillingData();
+	
+	@Test
+	void testIfRoomReserved() {
+		bill.setRoomReserved(true);
+		assertEquals(true, bill.isRoomReserved());
+	}
+	
+	@Test
+	void testDateHallReservedFor() {
+		bill.setDateHallReservedFor(new Date());
+		Date d = new Date();
+		assertEquals(d, bill.getDateHallReservedFor());
+	}
+	
+	@Test
+	void testBillOwnerName() {
+		bill.setName("Willy");;
+		assertEquals("Willy", bill.getName());
+	}
+
+	@Test
+	void testNumOfGuests() {
+		bill.setNumOfGuests(3);
+		assertNotEquals(5, bill.getNumOfGuests());
+	}
+}
