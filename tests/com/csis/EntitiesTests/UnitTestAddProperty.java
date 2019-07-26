c5f1e6ca2ce47b24b33eef7b0d437f433f16bb88
Amanpreet Singh <singha73@student.douglascollege.ca>
2019-07-25 22:11:18 -0700

    Add UnittestAddproperty class and unit case  test

---
+++ tests/com/csis/EntitiesTests/UnitTestAddProperty.java
@@ -0,0 +1,50 @@
+package com.csis.EntitiesTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.csis.Entities.AddProperty;
+
+class UnitTestAddProperty {
+
+	AddProperty addproperty = new AddProperty();
+	
+	@Test
+	void testItem() {
+		addproperty.setItem("food");
+		assertEquals("food" , addproperty.getItem());
+	}
+
+	@Test
+	void testType() {
+		addproperty.setType("Sea Food");
+		assertEquals("Sea Food" , addproperty.getType());
+	}
+	
+	/*@Test
+	void testQuantity() {
+		addproperty.setQuantity(1);
+		assertEquals(1 , addproperty.getQuantity());
+	}*/
+
+	@Test
+	void testPrice() {
+		addproperty.setPrice(12);
+		assertEquals(12 , addproperty.getPrice());
+	}
+	
+	
+	@Test
+	void testCategory() {
+		addproperty.setCategory("Veg");
+		assertEquals("Veg" , addproperty.getCategory());
+	}
+
+	@Test
+	void testUnitPrice() {
+		addproperty.setUnitprice(4);
+		assertEquals(4 , addproperty.getUnitprice());
+	}
+	
+}
